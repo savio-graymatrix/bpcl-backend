74159a165744660e9c0ff81ea8c27e3c3b0d4ad3
Arjun Pathak <arjun.pathak@graymatrix.com>
2025-07-01 16:54:04 +0530

    Feat: Added api route for review creation

--- bpcl/api/v1/endpoints/parse_review.py
+++ bpcl/api/v1/endpoints/parse_review.py
@@ -1,3 +1,18 @@
 from fastapi.routing import APIRouter
+from bpcl.langgraph.agents.compliance_agent import ComplianceAgent
+from bpcl.db.data_models.Bid import Bid
 
 router = APIRouter(prefix="/review")
+
+@router.post("/{bid_id}")
+async def parse_review(bid_id: str):
+    bid = await Bid.get(bid_id)
+    config = {
+        "configurable": {
+            "thread_id": 1,
+            "project_id": bid.project_id,
+            "bid_id": bid.id
+        }
+    }
+    result = await ComplianceAgent.compliance_agent({"messages": []}, config=config)
+    return result
