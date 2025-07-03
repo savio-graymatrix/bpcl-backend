0848369d212006d274e9efa08822fb3efe1d13fd
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-03 12:29:13 +0530

    Chore: Tags added to API

--- bpcl/db/data_models/Review.py
+++ bpcl/db/data_models/Review.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field, ConfigDict
-from typing import List, Literal
+from typing import List, Literal, Optional
 from beanie import Document
 from bson import ObjectId
 from datetime import datetime, timezone
@@ -8,6 +8,7 @@ from datetime import datetime, timezone
 class Review(Document):
     type: Literal["caution", "warning", "error"]
     reason: str
+    title: str
     review_set_id: ObjectId
     status: Literal["pending", "resolved", "rejected"]
 
@@ -39,15 +40,35 @@ class ReviewSet(Document):
 
 
 class ReviewResponse(BaseModel):
+    id: str
     type: Literal["caution", "warning", "error"]
     reason: str
+    title: str
+    review_set_id: str
+    status: Literal["pending", "resolved", "rejected"]
 
 
 class ReviewSetResponse(BaseModel):
-    application_id: ObjectId
+    application_id: str
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     reviews: List[ReviewResponse]
+    score: int
+    feedback: str
     model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+    )
+
+class UpdateReview(BaseModel):
+    id: Optional[str] = None
+    type: Optional[Literal["caution", "warning", "error"]] = None
+    reason: Optional[str] = None
+    review_set_id: Optional[str] = None
+    title: Optional[str] = None
+    status: Optional[Literal["pending", "resolved", "rejected"]] = None
+    model_config = ConfigDict(
+        json_encoders = {
+            ObjectId: str
+        },
         arbitrary_types_allowed=True,
     )
